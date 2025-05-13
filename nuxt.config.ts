1e615fe24d2380319b7a2f61fbae78d6d8bd4ac1
Jens Häuser <82268654+JensHouses@users.noreply.github.com>
2025-05-13 11:03:38 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,23 @@ export default defineNuxtConfig({
         ]
     }
   },
+  
+image: {
+  screens: {
+    sm: 320,
+    md: 640,
+    lg: 1024,
+    xl: 1280,
+    xxl: 1536
+  },
+  domains: [],
+  dir: 'public',
+  provider: 'static',
+  presets: {},
+  staticFilename: '[publicPath]/images/[name]-[hash][ext]',
+  baseURL: process.env.NUXT_APP_BASE_URL || '/' 
+},
+
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', '@pinia/nuxt'],
